275d746f475cb13552c4a4ca9208ee509caf0657
HRB-YuPai <875345836@qq.com>
2021-12-08 13:28:19 +0800

    Initial
    Initial

---
+++ omni_mi_x08a.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from mi_x08a device
+$(call inherit-product, device/xiaomi/mi_x08a/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mi_x08a
+PRODUCT_NAME := omni_mi_x08a
+PRODUCT_BRAND := XiaoAiTongXue
+PRODUCT_MODEL := X08A
+PRODUCT_MANUFACTURER := xiaomi
