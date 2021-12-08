43b5bc1116d402c4d7dc25f4f01a7e71e521945b
HRB-YuPai <875345836@qq.com>
2021-12-08 15:38:26 +0800

    updated_test

--- device.mk
+++ device.mk
@@ -1,40 +1,48 @@
-#
-# Copyright (C) 2021 The Android Open Source Project
-# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/xiaomi/mi_x08a
-# A/B
-AB_OTA_PARTITIONS += \
+
+# Define hardware platform
+PRODUCT_PLATFORM := mt8167
+
+AB_OTA_UPDATER := true
+TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)/bootctrl \
+
+# A/B partitions
+AB_OTA_PARTITIONS := \
     boot \
     system \
-    vendor
+    lk \
+    preloader
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+# Bootctrl
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client \
+    bootctl
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt8167
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt8167 \
-    libgptutils \
-    libz \
-    libcutils
-
+# A/B support
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
-    update_verifier \
-    update_engine_sideload
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload
+
+# Enable update engine sideloading by including the static version of the
+# boot_control HAL and its dependencies.
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    libgptutils \
+    libz \
+    libcutils
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.$(PRODUCT_PLATFORM)
